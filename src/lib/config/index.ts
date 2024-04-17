1391d86657614d7102bbb1779c9526f3ed47ea13
leonardo-marinho <leonardo.msnt@outlook.com>
2024-04-16 21:25:55 -0300

    Remove unused config files

--- src/lib/config/index.ts
+++
@@ -1,9 +0,0 @@
-import JwtConfig from './Jwt.config';
-import NodeConfig from './Node.config';
-
-class Config {
-  jwt = new JwtConfig();
-  node = new NodeConfig();
-}
-
-export default new Config();
